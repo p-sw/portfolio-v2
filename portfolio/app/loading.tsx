f530183257dc374f4c8a4288f9878c5eb723123b
sserve-kr <personal@sserve.me>
2023-05-27 17:00:14 +0900

    refactor(frontend): Change loading page

--- portfolio/app/loading.tsx
+++ portfolio/app/loading.tsx
@@ -1,9 +1,5 @@
-import "@/app/loading.css";
-
 export default function Loading() {
   return <div>
-    <div className={`center`}>
-      <p>Loading<span className={`d a`}>.</span><span className={`d b`}>.</span><span className={`d c`}>.</span></p>
-    </div>
+    <p>Loading...</p>
   </div>
 }
