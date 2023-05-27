c88e9c450dd035231bc378342f2ec43dd142ac7f
sserve-kr <personal@sserve.me>
2023-05-27 15:28:46 +0900

    feat(frontend): Add loading status page

---
+++ portfolio/app/loading.tsx
@@ -0,0 +1,9 @@
+import "@/app/loading.css";
+
+export default function Loading() {
+  return <div>
+    <div className={`center`}>
+      <p>Loading<span className={`d a`}>.</span><span className={`d b`}>.</span><span className={`d c`}>.</span></p>
+    </div>
+  </div>
+}
