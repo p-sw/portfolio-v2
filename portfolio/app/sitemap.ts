30c38360250edc2254e416afd863f9d874d3b165
sserve-kr <personal@sserve.me>
2023-05-28 12:56:49 +0900

    test(frontend): Add test for api data while deploying

--- portfolio/app/sitemap.ts
+++ portfolio/app/sitemap.ts
@@ -31,6 +31,7 @@ async function blogPosts(): Promise<sitemapUrl[]> {
         newbase.push({url: url(`blog/${item.id}`), lastModified: new Date(item.updated)})
       }
     }
+    console.log(data)
     if (!pageToken) return newbase;
   }
 }
